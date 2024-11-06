d070af959d216eadfc70fb5d040409cb1c68824f
andre <andreyshe22@gmail.com>
2024-11-06 21:00:14 +0500

    реализован тестовый gRPC-сервер - заглушка

---
+++ CityTalk.UserService/Api/Services/UserService.cs
@@ -0,0 +1,13 @@
+﻿using CommonLibrary.Protos;
+using Grpc.Core;
+
+namespace Api.Services
+{
+    public class UserService : User.UserBase
+    {
+        public override Task<UserProfile> GetUserProfile(GetUserProfileRequest request, ServerCallContext serverCallContext)
+        {
+            return Task.FromResult(new UserProfile { Username = "Test" });
+        }
+    }
+}
